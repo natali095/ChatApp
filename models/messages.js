ea495d926bdf95d667e09650f3c7978f0781ffa2
natali095 <kalininanataliia095@gmail.com>
2023-05-10 14:23:38 +0100

    sekond commit

--- models/messages.js
+++ models/messages.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose');
 
 const msgSchema = new mongoose.Schema({
   userName: { type: String, required: true },
-  msg: { type: String, required: true },
+  message: { type: String, required: true }, // Updated field name from 'msg' to 'message'
 });
 
-
 const Msg = mongoose.model('msg', msgSchema, 'message');
-module.exports = Msg;   
-
-
-
+module.exports = Msg;
   
   
--- server.js
+++ server.js
@@ -33,7 +33,24 @@ io.on('connection', (socket) => {
   socket.on('message', (msg) => {
       socket.broadcast.emit('message-broadcast', {message: msg, userName: userName});
   })
-
+  socket.on('message', (msg) => {
+    // Create a new instance of the Msg model
+    const newMessage = new Msg({
+      userName: userName,
+      message: msg
+    });
+  
+    // Save the message to the database
+    newMessage.save()
+      .then(savedMessage => {
+        // Emit the saved message to all connected clients
+        io.emit('message-broadcast', { message: savedMessage.message, userName: savedMessage.userName });
+      })
+      .catch(error => {
+        console.error('Error saving message:', error);
+      });
+  });
+  
   socket.on('disconnect', (reason) => {
       removeUser(userName, socket.id);
   })
@@ -60,70 +77,3 @@ http.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running ${process.env.PORT || 3000}`);
 });
 
-
-// const express = require('express');
-// const path = require("path");
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const mongo = require("mongoose");
-
-
-
-
-
-
-// const app = express();
-// const http = require('http').createServer(app);
-
-// const io = require('socket.io')(http, {
-//     cors: {
-//         origin: '*'
-//     }
-// });
-
-// app.get('/', (req, res) => {
-//     res.send('Hello, I`m Nataliia`s server');
-// })
-
-
-// let userList = new Map();
-// io.on('connection', (socket) => {
-//   let userName = socket.handshake.query.userName;
-//   addUser(userName, socket.id);
-
-//   socket.broadcast.emit('user-list', [...userList.keys()]);
-//   socket.emit('user-list', [...userList.keys()]);
-
-//   socket.on('message', (msg) => {
-//       socket.broadcast.emit('message-broadcast', {message: msg, userName: userName});
-//   })
-
-//   socket.on('disconnect', (reason) => {
-//       removeUser(userName, socket.id);
-//   })
-// });
-
-
-
-
-// function addUser(userName, id) {
-//     if (!userList.has(userName)) {
-//         userList.set(userName, new Set(id));
-//     } else {
-//         userList.get(userName).add(id);
-//     }
-// }
-// function removeUser(userName, id) {
-//   if (userList.has(userName)) {
-//       let userIds = userList.get(userName);
-//       userIds.delete(id);
-//       if (userIds.size == 0) {
-//           userList.delete(userName);
-//       }
-//   }
-// }
-
-
-// http.listen(process.env.PORT || 3000, () => {
-//     console.log(`Server is running ${process.env.PORT || 3000}`);
-// });
